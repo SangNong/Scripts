db66b26f34ded0746b7cdd9a0edcd0f70f7353e6
Sang Nong <sn2257@nyu.edu>
2017-11-13 19:56:40 -0500

    Car path and popcornSpawn point

---
+++ popcornSpawn.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class popcornSpawn : MonoBehaviour
+{
+	public GameObject popcornPrefab;
+	public Transform spawnPoint;
+
+	void Update (){
+		GameObject popcornInstance;
+		popcornInstance = Instantiate(popcornPrefab, spawnPoint.position, spawnPoint.rotation) as GameObject;
+		popcornInstance.GetComponent<Rigidbody> ().AddForce(spawnPoint.forward * 500);
+
+	}
+	}
